e88382538473d6a5be36bc01b613d9b834e4fcac
Dixy-05 <ddbarahona89@gmail.com>
2021-09-07 08:58:24 -0600

    new fetch route,new form styling

--- src/App.js
+++ src/App.js
@@ -5,15 +5,121 @@ import AppNavs from './modules/navs';
 import Users from './modules/users';
 
 function App() {
+  const handleRegister = () => {
+    const registerForm = document.querySelector('#Register-Form');
+    const email = registerForm['register-email'].value;
+    const password = registerForm['register-password'].value;
+    registerForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      console.log('email:', email, 'password:', password);
+      fetch('http://localhost:8081/api/register', {
+        method: 'POST',
+        credentials: 'same-origin',
+        // withCredentials: true,
+        cache: 'no-cache',
+        mode: 'cors',
+        headers: {
+          'content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      })
+        .then((res) => res.json())
+        .then((json) => {
+          console.log('the return', json);
+          json && alert(`User was succesfully registered`);
+        })
+        .catch((err) => console.log(err));
+    });
+    registerForm['register-email'].value = '';
+    registerForm['register-password'].value = '';
+  };
+  const handleLogin = () => {
+    const loginForm = document.querySelector('#Login-Form');
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+    loginForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      fetch('http://localhost:8081/api/login', {
+        method: 'POST',
+        headers: {
+          'content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+        cache: 'no-cache',
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      })
+        .then(async (res) => res.json())
+        .then((json) => console.log(json))
+        .catch((err) => console.log(err));
+    });
+    loginForm['login-email'].value = '';
+    loginForm['login-password'].value = '';
+  };
+
+  const handleDelete = () => {
+    const deleteForm = document.querySelector('#Delete-Form');
+    const email = deleteForm['delete-email'].value;
+    console.log('EMAIL-INPUT:', email);
+    deleteForm.addEventListener('submit', async (e) => {
+      e.preventDefault();
+      if (email) {
+        document.querySelector('#warning').innerHTML = '';
+        try {
+          const deleteUser = await fetch(
+            `http://localhost:8081/api/users/${email}`,
+            {
+              method: 'DELETE',
+              headers: {
+                'content-Type': 'application/json',
+                Accept: 'application/json',
+              },
+            }
+          );
+          const resJson = await deleteUser.json();
+          console.log(resJson);
+        } catch (err) {
+          console.log(err);
+        }
+        deleteForm.reset();
+      } else {
+        document.querySelector('#warning').innerHTML =
+          'You must enter a valid email !!';
+        alert('You must enter e valid email !!');
+      }
+    });
+  };
   return (
     <React.Fragment>
       <AppNavbar />
       <div className="App">
         <AppNavs />
-        <Users />
+        <Users
+          login={handleLogin}
+          register={handleRegister}
+          delete={handleDelete}
+        />
       </div>
+      {/* <div>
+        <form>
+          <label>
+            Choose date:<input type="date"></input>
+          </label>
+        </form>
+        <button onClick={doThis} id="date">
+          fetch
+        </button>
+      </div> */}
     </React.Fragment>
   );
 }
 
 export default App;
+
+// email: mani@gmail.com password: Mal2345To45
--- src/modules/users.js
+++ src/modules/users.js
@@ -1,74 +1,105 @@
 import React from 'react';
-import { Form, Row, Button, Container, Col, InputGroup } from 'react-bootstrap';
 import { useState } from 'react';
+import { Form, Button, Container, Alert } from 'react-bootstrap';
 
-const Users = () => {
-  const [validated, setValidated] = useState(false);
-  const handleSubmit = (event) => {
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
-    setValidated(true);
+const Users = (props) => {
+  const [show, setShow] = useState(false);
+
+  const deleteUser = (e) => {
+    props.delete(e);
+    setShow(false);
   };
 
   return (
     <React.Fragment>
-      <h2 id="new-user">Add New User</h2>
       <Container>
-        <Form noValidate validated={validated} onSubmit={handleSubmit}>
-          <Row className="mb-3">
-            <Form.Group as={Col} md="4" controlId="validationFirstName">
-              <Form.Label>First name</Form.Label>
-              <Form.Control
-                required
-                type="text"
-                placeholder="Enter First name"
-                // defaultValue="Mark"
-              />
-              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} md="4" controlId="validationLastName">
-              <Form.Label>Last name</Form.Label>
-              <Form.Control
-                required
-                type="text"
-                placeholder="Enter Last name"
-                // defaultValue="Otto"
-              />
-              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} md="4" controlId="validationEmail">
-              <Form.Label>Email</Form.Label>
-              <InputGroup hasValidation>
-                <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-                <Form.Control
-                  type="email"
-                  placeholder="Enter email"
-                  aria-describedby="inputGroupPrepend"
-                  required
-                />
-                <Form.Control.Feedback type="invalid">
-                  Please enter a valid email
-                </Form.Control.Feedback>
-              </InputGroup>
-            </Form.Group>
-          </Row>
-          <Row className="mb-3">
-            <Form.Group as={Col} md="3" controlId="validationPhone">
-              <Form.Label>Phone</Form.Label>
-              <Form.Control type="number" placeholder="phone" required />
-              <Form.Control.Feedback type="invalid">
-                Please provide a valid phone.
-              </Form.Control.Feedback>
-            </Form.Group>
-          </Row>
-          <Form.Group className="mb-3"></Form.Group>
-          <Button type="submit">Create User</Button>
+        <Form id="Register-Form">
+          <h1>Register User</h1>
+          <Form.Group className="mb-3" controlId="register-email">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              // id="register-email"
+              type="email"
+              placeholder="Enter email"
+            />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="register-password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              // id="register-password"
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit" onClick={props.register}>
+            Submit
+          </Button>
         </Form>
       </Container>
-      <ul id="addUserResponse"></ul>
+      <hr />
+      <Container>
+        <Form id="Login-Form">
+          <h1>Login User</h1>
+          <Form.Group className="mb-3" controlId="login-email">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="login-password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Button variant="primary" type="submit" onClick={props.login}>
+            Submit
+          </Button>
+        </Form>
+      </Container>
+      <hr />
+      <Container>
+        <Form id="Delete-Form">
+          <h1>Delete User</h1>
+          <Form.Group className="mb-3" controlId="delete-email">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              autoComplete="off"
+              type="email"
+              placeholder="Enter email"
+            />
+            <Form.Text id="warning" className="text-muted"></Form.Text>
+          </Form.Group>
+          <Alert show={show} variant="light">
+            <Alert.Heading>Are you sure you want to delete user?</Alert.Heading>
+            <hr />
+            <div className="d-flex justify-content-center">
+              <Button
+                type="submit"
+                onClick={(e) => deleteUser(e)}
+                variant="outline-danger"
+              >
+                Yes
+              </Button>
+              <Button
+                className="ms-2"
+                onClick={() => setShow(false)}
+                variant="outline-primary"
+              >
+                No
+              </Button>
+            </div>
+          </Alert>
+          <Button type="button" variant="danger" onClick={() => setShow(true)}>
+            Delete
+          </Button>
+        </Form>
+      </Container>
+      <Container></Container>
     </React.Fragment>
   );
 };
