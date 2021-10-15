63729790d2417ebefc8ff4a5afa9321e6f2d747a
Dixy-05 <ddbarahona89@gmail.com>
2021-10-15 08:45:45 -0600

    store.js fix 2

--- src/stores/store.js
+++ src/stores/store.js
@@ -2,6 +2,6 @@ import { createStore } from 'redux';
 import RootReducer from '../reducers/rootReducer';
 
 export const store = createStore(
-  RootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  RootReducer
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
